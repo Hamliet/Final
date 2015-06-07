44e0b6dd40f251b746279f5beb3363f6b614804b
Hamliet <hamliet@hanyang.ac.kr>
2015-06-08 00:48:46 +0900

    ver6

--- src/Trade.java
+++ src/Trade.java
@@ -9,4 +9,17 @@ public class Trade implements Serializable{
 	int export_sum; //수출금액
 	int imports; //수입건수
 	int import_sum; //수입금액  단위:천불(USD 1,000)
+	public String toString(){
+		return ("< "+serial_num+" >　("+nation+")　"+date.substring(0,4)+"년"+date.substring(5)+"월　수출건수:"+exports+"　수출금액:"+export_sum+"　수입건수:"+imports+"　수입금액:"+import_sum+"　무역수지:"+(export_sum - import_sum)+"\n");
+	}
+	public Trade(){
+		date = null;
+		nation = null;
+		serial_num=0;
+		exports=0; //수출건수
+		export_sum=0; //수출금액
+		imports=0; //수입건수
+		import_sum=0; //수입금액  단위:천불(USD 1,000)
+	}
+		
 }
--- src/TradeManagerSwing.java
+++ src/TradeManagerSwing.java
@@ -11,14 +11,12 @@ import java.awt.event.KeyListener;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -31,6 +29,7 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 
@@ -194,7 +193,7 @@ class MyFrame extends JFrame implements ActionListener, KeyListener, FocusListen
 			if(t.size() !=0){
 				ta.setText("");
 				for(i=0;i<t.size();i++){
-					ta.append("< "+t.get(i).serial_num+" >　("+t.get(i).nation+")　"+t.get(i).date.substring(0,4)+"년"+t.get(i).date.substring(5)+"월　수출건수:"+t.get(i).exports+"　수출금액:"+t.get(i).export_sum+"　수입건수:"+t.get(i).imports+"　수입금액:"+t.get(i).import_sum+"　무역수지:"+(t.get(i).export_sum - t.get(i).import_sum+"\n"));
+					ta.append(t.get(i).toString());
 				}
 			}
 			else{
@@ -206,7 +205,7 @@ class MyFrame extends JFrame implements ActionListener, KeyListener, FocusListen
 		
 		if(e.getSource() == button4){
 			if(t.size() !=0){
-				ta.setText("");
+
 				Button4_Frame b4 = new Button4_Frame(MyFrame.this);
 			}
 			else{
@@ -540,7 +539,7 @@ class Button2_Frame extends JDialog implements ActionListener, KeyListener{
 	HashSet<String> sample2 = new HashSet<String>();
 	public Button2_Frame(MyFrame MyFrame){
 		setTitle("Edit Trade");
-		setSize(480,280);
+		setSize(450,250);
 		this.MyFrame = MyFrame;
 		label1= new JLabel("삭제");
 		label2= new JLabel("날짜");
@@ -693,9 +692,105 @@ class Button2_Frame extends JDialog implements ActionListener, KeyListener{
 class Button4_Frame extends JDialog implements ActionListener, KeyListener{
 	MyFrame MyFrame;
 	Choice nation_choice;
-	Choice date_choice;
+	JLabel label1;
+	JLabel label2;
+	JLabel label3;
+	JLabel label4;
+	JLabel label5;
+	JLabel label6;
+	JLabel label7;
+	JLabel label8;
+	JLabel label9;
+	JTextField tf1;
+	JTextField tf2;
+	JTextField tf3;
+	JTextField tf4;
+	JTextArea ta;
+	JPanel p1;
+	JPanel p2;
+	JPanel p3;
+	JPanel p4;
+	JPanel p5;
+	JButton Add;
+	JButton Search;
+	JButton CLOSE;
+	int all=-1;
+	int all_date=-1;
+	
+	HashSet<String> sample = new HashSet<String>();
+
 	public Button4_Frame(MyFrame MyFrame){
 		this.MyFrame = MyFrame;
+		setTitle("Add Trade");
+		setSize(340,240);
+		this.MyFrame = MyFrame;
+		label1= new JLabel("기간");
+		label2= new JLabel("년");
+		label3= new JLabel("월");
+		label4= new JLabel("~");
+		label5= new JLabel("년");
+		label6= new JLabel("월");
+		label7= new JLabel("국가");
+		label8= new JLabel("검색대상");
+		label9= new JLabel("　　　　　　　　　　　　　(기간 미입력시 전체검색)");
+		tf1 = new JTextField(4);
+		tf2 = new JTextField(2);
+		tf3 = new JTextField(4);
+		tf4 = new JTextField(2);
+		ta = new JTextArea();
+		ta.disable();
+		p1 = new JPanel();
+		p2 = new JPanel();
+		p3 = new JPanel();
+		p4 = new JPanel();
+		p5 = new JPanel();
+		nation_choice = new Choice();
+		for(int i=0;i<this.MyFrame.t.size();i++){ //콤보박스 설정
+			sample.add(this.MyFrame.t.get(i).nation);
+		}
+		Iterator<String> iterator = sample.iterator();
+		nation_choice.add("전체");
+		while(iterator.hasNext()){
+			 String nation = iterator.next();
+			 nation_choice.add(nation);
+		}
+		Add = new JButton("Add");
+		Search = new JButton("SEARCH");
+		CLOSE = new JButton("CLOSE");
+		
+		
+		
+		setLayout(new GridLayout(5,1));
+		p1.add(label1);
+		p1.add(tf1);
+		p1.add(label2);
+		p1.add(tf2);
+		p1.add(label3);
+		p1.add(label4);
+		p1.add(tf3);
+		p1.add(label5);
+		p1.add(tf4);
+		p1.add(label6);
+		p2.add(label7);
+		p2.add(nation_choice);
+		p2.add(Add);
+		p3.add(Search);
+		p3.add(CLOSE);
+		p4.setLayout(new BorderLayout ());
+		p4.add(label8,"West");
+		p4.add(ta,"Center");
+		p5.add(label9);
+		add(p2,0);
+		add(p4,1);
+		add(p1,2);
+		add(p5,3);
+		add(p3,4);
+		Add.addActionListener(this);
+		Search.addActionListener(this);
+		CLOSE.addActionListener(this);
+		setVisible(true);
+		
+		
 	}
 	
 
@@ -716,6 +811,110 @@ class Button4_Frame extends JDialog implements ActionListener, KeyListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == Add && all==-1){
+			String choosen = nation_choice.getSelectedItem().toString();
+			if(choosen.equals("전체")){ //전체가 선택되면 비활성화
+				all=1;
+			}
+			nation_choice.remove(nation_choice.getSelectedIndex());
+			ta.append(choosen+" ");
+		}
+
+		if(e.getSource() == Search){
+			if(ta.getText().length() ==0){
+				MyFrame.ta.append("국가를 선택하지 않았습니다."); //국가선택을 안했을 경우
+			}
+			else{
+				int find=-1; //찾는 값이 없을경우 알려주기 위한 키
+				if(tf1.getText().length() ==0 &&  tf2.getText().length() ==0 && tf3.getText().length() ==0 && tf4.getText().length() ==0){
+					all_date=1;
+				}//입력값이 없으면 전체검색
+
+				else if(tf1.getText().length() ==0 ||  tf2.getText().length() ==0 || tf3.getText().length() ==0 || tf4.getText().length() ==0){
+					all_date=0;
+
+				}//중간에 빈칸이 있으면 검색불가	
+				
+				if(all_date==0){
+					MyFrame.ta.append("기간을 올바르게 입력하지 않았습니다.\n");
+				}
+				
+				else{
+					String s1= tf1.getText();
+					String s2= tf2.getText();
+					String s3= tf3.getText();
+					String s4= tf4.getText();
+					if(s2.length()==1){ //날짜입력 유용성 확보
+						s1= "0"+s1;
+					}
+					if(s4.length()==1){ //날짜입력 유용성 확보
+						s4="0"+s4;
+					}
+					int s_date = Integer.parseInt(s1+s2);
+					int e_date = Integer.parseInt(s3+s4);
+					if(e_date<s_date || Integer.parseInt(tf1.getText())>9999 || Integer.parseInt(tf1.getText())>9999 ||Integer.parseInt(tf2.getText())>12 || Integer.parseInt(tf4.getText()) >12){
+						MyFrame.ta.append("기간을 올바르게 입력하지 않았습니다.\n");
+					}
+					else{
+						String [] list = ta.getText().split(" ");
+						if(all_date==1 && all==1){ //모든날짜, 모든국가일때
+							MyFrame.ta.append("\n<<검색결과>>\n");
+							for(int i=0;i<MyFrame.t.size();i++){
+								MyFrame.ta.append(MyFrame.t.get(i).toString());
+								find=1;
+							}					
+						}
+						else if(all_date==1 && all!=1){ //모든날짜, 선택국가일때
+							MyFrame.ta.append("\n<<검색결과>>\n");
+							for(int j=0;j<list.length;j++){
+								for(int i=0;i<MyFrame.t.size();i++){
+									if(list[j].equals(MyFrame.t.get(i).nation)){
+										MyFrame.ta.append(MyFrame.t.get(i).toString());
+										find=1;
+									}
+								}
+							}	
+						}
+						else if(all_date==1 && all!=1){ //선택날짜, 모든국가일때
+							MyFrame.ta.append("\n<<검색결과>>\n");
+
+							for(int i=0;i<MyFrame.t.size();i++){
+								if(s_date <= Integer.parseInt(MyFrame.t.get(i).date.substring(0,4)+MyFrame.t.get(i).date.substring(5)) && Integer.parseInt(MyFrame.t.get(i).date.substring(0,4)+MyFrame.t.get(i).date.substring(5)) <= e_date ){	
+									MyFrame.ta.append(MyFrame.t.get(i).toString());
+									find=1;
+								}
+							}	
+						}
+						else{ //선택국가, 선택날짜일때
+
+							MyFrame.ta.append("\n<<검색결과>>\n");
+							for(int i=0;i<MyFrame.t.size();i++){
+								for(int j=0;j<list.length;j++){
+									if(s_date <= Integer.parseInt(MyFrame.t.get(i).date.substring(0,4)+MyFrame.t.get(i).date.substring(5)) && Integer.parseInt(MyFrame.t.get(i).date.substring(0,4)+MyFrame.t.get(i).date.substring(5)) <= e_date ){	
+										if(list[j].equals(MyFrame.t.get(i).nation)){
+											MyFrame.ta.append(MyFrame.t.get(i).toString());
+											find=1;
+										}
+									}
+								}	
+							}
+						}
+						if(find!=1){
+							MyFrame.ta.append("조건에 맞는 값이 없습니다.\n");
+						}	
+					}	
+				}
+
+				
+			}	
+			dispose();	
+		}
+
+		if(e.getSource() == CLOSE){
+			dispose();
+		}
+
+		
 
 	}
 	
